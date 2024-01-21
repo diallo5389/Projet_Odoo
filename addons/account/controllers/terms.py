1269d0afaaffde825858afb8190594f703b59989
diallo5389 <realbach41@gmail.com>
2024-01-21 20:06:35 +0100

    Odoo.log changes A suivre Je reviendrai vers vous

--- addons/account/controllers/terms.py
+++ addons/account/controllers/terms.py
@@ -27,3 +27,6 @@ class TermsController(http.Controller):
             'company': request.env.company
         }
         return request.render("account.account_terms_conditions_page", values)
+    
+
+#Texte pour tester le GIT
