017701fc26bfb3524cec99181c02583ccdc8df99
diallo5389 <realbach41@gmail.com>
2024-01-26 00:33:17 +0100

    Création de droits d'accès     Création et configuration du fichier ir.model.access.csv     en deffinissant les droits d'accès au modèle "domaines_proprietes"     Correction du champ " date_availability" pour faire disparaitre les logs "2024-01-25 22:49:14,607 204265 WARNING domaines-1 odoo.fields: Field domaines_proprietes.date_availability: unknown parameter 'day', if this is an actual parameter you may want to override the method _valid_field_parameter on the relevant model in order to allow it ", corrige ça "date_availability = fields.Date(year=2024,month=1,day=1)"

--- addons/domaines/models/domaines_proprietes.py
+++ addons/domaines/models/domaines_proprietes.py
@@ -7,7 +7,8 @@ class ModelProprietes(models.Model):
     name =  fields.Char(string='Nom',required=True) #required=True fait en sorte que ce champs ne puisse pas etre vide
     description = fields.Text(string='Description')
     postcode = fields.Char(string='Postcode')
-    date_availability = fields.Date(year=2024,month=1,day=1) # La date doit obligatoirement etre initialisée
+    #date_availability = fields.Date(year=2024,month=1,day=1) # La date doit obligatoirement etre initialisée
+    date_availability = fields.Date(default=fields.Date.today)
     expected_price = fields.Float(string='Expected Price')
     selling_price = fields.Float(string='Selling Price',required=True) 
     bedrooms = fields.Integer(string='Bedrooms')
